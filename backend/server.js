cb690805d0fb3c3568188de8b83e59687a794eba
kylejblu <blundellsignups@gmail.com>
2022-06-14 14:45:00 -0700

    Initial Rest API for goals

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const PORT = process.env.PORT || 5000;
-const {errorHandler} = require('./middleware/errorMiddleware')
+const { errorHandler } = require('./middleware/errorMiddleware');
+const colors = require('colors');
+const connectDB = require('./config/db');
 const app = express();
 
-
+connectDB();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(errorHandler)
-
+app.use(errorHandler);
 
 app.listen(5000, () => {
   console.log('Server started on port ' + PORT);
